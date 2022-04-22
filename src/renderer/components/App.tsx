d0e2ada8bf861514b76d5c397cb97f3a57af6acb
bigfarts <bigfarts@punymail.com>
2022-04-22 00:46:00 -0700

    dump match settings

--- src/renderer/components/App.tsx
+++ src/renderer/components/App.tsx
@@ -1,4 +1,10 @@
 import AppBar from "@mui/material/AppBar";
+import Stack from "@mui/material/Stack";
+import Chip from "@mui/material/Chip";
+import TextField from "@mui/material/TextField";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import Autocomplete from "@mui/material/Autocomplete";
 import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
 import Toolbar from "@mui/material/Toolbar";
@@ -7,24 +13,19 @@ import ThemeProvider from "@mui/system/ThemeProvider";
 import React from "react";
 import theme from "../theme";
 
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
 export default function App(): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <Box sx={{ display: "flex" }}>
         <CssBaseline />
-        <AppBar>
-          <Toolbar variant="dense">
-            <Typography
-              component="h1"
-              variant="h6"
-              color="inherit"
-              noWrap
-              sx={{ flexGrow: 1 }}
-            >
-              Tango
-            </Typography>
-          </Toolbar>
-        </AppBar>
         <Box
           component="main"
           sx={{
@@ -37,7 +38,220 @@ export default function App(): JSX.Element {
             overflow: "auto",
           }}
         >
-          hello
+          <Box sx={{ px: 1, py: 1 }}>
+            <Autocomplete
+              disablePortal
+              size="small"
+              id="combo-box-demo"
+              options={[{ label: "Cannon A", value: 0 }]}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="Add a chip (24 remaining)..."
+                  variant="standard"
+                />
+              )}
+            />
+
+            <TableContainer sx={{ py: 1 }}>
+              <Table size="small">
+                <TableBody>
+                  <TableRow>
+                    <TableCell width="0px">
+                      <img
+                        src="https://bntools.murk.land/chiplibrary/images/6/chipicons/s1.png"
+                        width="28"
+                        height="28"
+                        style={{ imageRendering: "pixelated" }}
+                      />
+                    </TableCell>
+                    <TableCell width="100%">
+                      <strong>Cannon</strong>
+                    </TableCell>
+                    <TableCell width="0px">
+                      <img
+                        src="https://bntools.murk.land/chiplibrary/images/6/elements/null.png"
+                        width="28"
+                        height="28"
+                        style={{ imageRendering: "pixelated" }}
+                      />
+                    </TableCell>
+                    <TableCell>
+                      <Select size="small" variant="standard" value={0}>
+                        <MenuItem value={0}>A</MenuItem>
+                        <MenuItem value={1}>B</MenuItem>
+                        <MenuItem value={2}>C</MenuItem>
+                        <MenuItem value={26}>﹡</MenuItem>
+                      </Select>
+                    </TableCell>
+                    <TableCell>
+                      <span style={{ whiteSpace: "nowrap" }}>6 MB</span>
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell width="0px">
+                      <img
+                        src="https://bntools.murk.land/chiplibrary/images/6/chipicons/s1.png"
+                        width="28"
+                        height="28"
+                        style={{ imageRendering: "pixelated" }}
+                      />
+                    </TableCell>
+                    <TableCell width="100%">
+                      <strong>Cannon</strong>
+                    </TableCell>
+                    <TableCell width="0px">
+                      <img
+                        src="https://bntools.murk.land/chiplibrary/images/6/elements/null.png"
+                        width="28"
+                        height="28"
+                        style={{ imageRendering: "pixelated" }}
+                      />
+                    </TableCell>
+                    <TableCell>
+                      <Select size="small" variant="standard" value={0}>
+                        <MenuItem value={0}>A</MenuItem>
+                        <MenuItem value={1}>B</MenuItem>
+                        <MenuItem value={2}>C</MenuItem>
+                        <MenuItem value={26}>﹡</MenuItem>
+                      </Select>
+                    </TableCell>
+                    <TableCell>
+                      <span style={{ whiteSpace: "nowrap" }}>6 MB</span>
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell width="0px">
+                      <img
+                        src="https://bntools.murk.land/chiplibrary/images/6/chipicons/s1.png"
+                        width="28"
+                        height="28"
+                        style={{ imageRendering: "pixelated" }}
+                      />
+                    </TableCell>
+                    <TableCell width="100%">
+                      <strong>Cannon</strong>
+                    </TableCell>
+                    <TableCell width="0px">
+                      <img
+                        src="https://bntools.murk.land/chiplibrary/images/6/elements/null.png"
+                        width="28"
+                        height="28"
+                        style={{ imageRendering: "pixelated" }}
+                      />
+                    </TableCell>
+                    <TableCell>
+                      <Select size="small" variant="standard" value={0}>
+                        <MenuItem value={0}>A</MenuItem>
+                        <MenuItem value={1}>B</MenuItem>
+                        <MenuItem value={2}>C</MenuItem>
+                        <MenuItem value={26}>﹡</MenuItem>
+                      </Select>
+                    </TableCell>
+                    <TableCell>
+                      <span style={{ whiteSpace: "nowrap" }}>6 MB</span>
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell width="0px">
+                      <img
+                        src="https://bntools.murk.land/chiplibrary/images/6/chipicons/s1.png"
+                        width="28"
+                        height="28"
+                        style={{ imageRendering: "pixelated" }}
+                      />
+                    </TableCell>
+                    <TableCell width="100%">
+                      <strong>Cannon</strong>
+                    </TableCell>
+                    <TableCell width="0px">
+                      <img
+                        src="https://bntools.murk.land/chiplibrary/images/6/elements/null.png"
+                        width="28"
+                        height="28"
+                        style={{ imageRendering: "pixelated" }}
+                      />
+                    </TableCell>
+                    <TableCell>
+                      <Select size="small" variant="standard" value={1}>
+                        <MenuItem value={0}>A</MenuItem>
+                        <MenuItem value={1}>B</MenuItem>
+                        <MenuItem value={2}>C</MenuItem>
+                        <MenuItem value={26}>﹡</MenuItem>
+                      </Select>
+                    </TableCell>
+                    <TableCell>
+                      <span style={{ whiteSpace: "nowrap" }}>6 MB</span>
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell width="0px">
+                      <img
+                        src="https://bntools.murk.land/chiplibrary/images/6/chipicons/s1.png"
+                        width="28"
+                        height="28"
+                        style={{ imageRendering: "pixelated" }}
+                      />
+                    </TableCell>
+                    <TableCell width="100%">
+                      <strong>Cannon</strong>
+                    </TableCell>
+                    <TableCell width="0px">
+                      <img
+                        src="https://bntools.murk.land/chiplibrary/images/6/elements/null.png"
+                        width="28"
+                        height="28"
+                        style={{ imageRendering: "pixelated" }}
+                      />
+                    </TableCell>
+                    <TableCell>
+                      <Select size="small" variant="standard" value={1}>
+                        <MenuItem value={0}>A</MenuItem>
+                        <MenuItem value={1}>B</MenuItem>
+                        <MenuItem value={2}>C</MenuItem>
+                        <MenuItem value={26}>﹡</MenuItem>
+                      </Select>
+                    </TableCell>
+                    <TableCell>
+                      <span style={{ whiteSpace: "nowrap" }}>6 MB</span>
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell width="0px">
+                      <img
+                        src="https://bntools.murk.land/chiplibrary/images/6/chipicons/s1.png"
+                        width="28"
+                        height="28"
+                        style={{ imageRendering: "pixelated" }}
+                      />
+                    </TableCell>
+                    <TableCell width="100%">
+                      <strong>Cannon</strong>
+                    </TableCell>
+                    <TableCell width="0px">
+                      <img
+                        src="https://bntools.murk.land/chiplibrary/images/6/elements/null.png"
+                        width="28"
+                        height="28"
+                        style={{ imageRendering: "pixelated" }}
+                      />
+                    </TableCell>
+                    <TableCell>
+                      <Select size="small" variant="standard" value={1}>
+                        <MenuItem value={0}>A</MenuItem>
+                        <MenuItem value={1}>B</MenuItem>
+                        <MenuItem value={2}>C</MenuItem>
+                        <MenuItem value={26}>﹡</MenuItem>
+                      </Select>
+                    </TableCell>
+                    <TableCell>
+                      <span style={{ whiteSpace: "nowrap" }}>6 MB</span>
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Box>
         </Box>
       </Box>
     </ThemeProvider>
