bb998c814f2f0a2b9f18d050481814adbca508ab
bigfarts <bigfarts@punymail.com>
2022-04-22 00:58:17 -0700

    fix array2d types

--- src/array2d.ts
+++ src/array2d.ts
@@ -18,7 +18,7 @@ array2d.from = function <T>(data: Array<T>, nrows: number, ncols: number) {
 };
 
 array2d.full = function <T>(v: T, nrows: number, ncols: number) {
-  const arr2d = array2d(nrows, ncols);
+  const arr2d = array2d<T>(nrows, ncols);
   arr2d.fill(v, 0, nrows * ncols);
   return arr2d;
 };
@@ -34,7 +34,7 @@ array2d.subarray = function <T>(
   nrows: number,
   ncols: number
 ) {
-  const subarr2d = array2d(nrows, ncols);
+  const subarr2d = array2d<T>(nrows, ncols);
   for (let i = 0; i < nrows; ++i) {
     for (let j = 0; j < ncols; ++j) {
       subarr2d[i * ncols + j] = arr2d[(top + i) * arr2d.ncols + (left + j)];
@@ -44,13 +44,13 @@ array2d.subarray = function <T>(
 };
 
 array2d.transpose = function <T>(arr2d: Array2D<T>) {
-  const transposed = array2d(arr2d.ncols, arr2d.nrows);
+  const transposed = array2d<T>(arr2d.ncols, arr2d.nrows);
   for (let i = 0; i < arr2d.nrows; ++i) {
     for (let j = 0; j < arr2d.ncols; ++j) {
       transposed[j * transposed.ncols + i] = arr2d[i * arr2d.ncols + j];
     }
   }
-  return transposed as Array2D<T>;
+  return transposed;
 };
 
 array2d.flipRowsInplace = function <T>(arr2d: Array2D<T>) {
@@ -94,7 +94,7 @@ array2d.row = function <T>(arr2d: Array2D<T>, i: number) {
 };
 
 array2d.col = function <T>(arr2d: Array2D<T>, j: number) {
-  const col = new Array(arr2d.nrows);
+  const col = new Array<T>(arr2d.nrows);
   for (let i = 0; i < arr2d.nrows; ++i) {
     col[i] = arr2d[i * arr2d.ncols + j];
   }
