fdf2cfa362c609536634453318af932f04e76328
bigfarts <bigfarts@punymail.com>
2022-04-21 19:18:53 -0700

    fix keymaptool

--- src/renderer/components/App.tsx
+++ src/renderer/components/App.tsx
@@ -1,13 +1,44 @@
-import { Box, CssBaseline, ThemeProvider } from "@mui/material";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import ThemeProvider from "@mui/system/ThemeProvider";
 import React from "react";
 import theme from "../theme";
 
 export default function App(): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Box>
-        <main></main>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar>
+          <Toolbar variant="dense">
+            <Typography
+              component="h1"
+              variant="h6"
+              color="inherit"
+              noWrap
+              sx={{ flexGrow: 1 }}
+            >
+              Tango
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === "light"
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: "100vh",
+            overflow: "auto",
+          }}
+        >
+          hello
+        </Box>
       </Box>
     </ThemeProvider>
   );
