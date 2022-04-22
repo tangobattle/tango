30ba41b7673978fc80264fe2eb5fa18694275bb2
bigfarts <bigfarts@punymail.com>
2022-04-22 00:57:04 -0700

    be more explicit in starting/stopping core

--- src/ipc/react.tsx
+++ src/ipc/react.tsx
@@ -1,34 +1,46 @@
 import * as ipc from ".";
 import React from "react";
 
-export function useCore(args: ipc.Args) {
+export function useCore() {
   const [state, setState] = React.useState<ipc.State | null>(null);
   const [stderr, setStderr] = React.useState<string[]>([]);
   const [exitStatus, setExitStatus] = React.useState<ipc.ExitStatus | null>(
     null
   );
 
-  React.useEffect(() => {
-    const abortController = new AbortController();
-    const core = new ipc.Core(args, {
-      signal: abortController.signal,
-    });
-    core.on("exit", (exitStatus) => {
-      setExitStatus(exitStatus);
-    });
-    core.on("state", (state) => {
-      setState(state);
-    });
-    core.on("stderr", (stderr) => {
-      setStderr((lines) => {
-        lines.push(stderr);
-        return lines;
-      });
-    });
-    return () => {
-      abortController.abort();
-    };
-  }, [args]);
+  const abortControllerRef = React.useRef<AbortController>();
 
-  return { state, stderr, exitStatus };
+  return {
+    start: (args: ipc.Args) => {
+      if (abortControllerRef.current != null) {
+        throw new Error("core already started");
+      }
+
+      const abortController = new AbortController();
+      abortControllerRef.current = abortController;
+      const core = new ipc.Core(args, {
+        signal: abortController.signal,
+      });
+      core.on("exit", (exitStatus) => {
+        setExitStatus(exitStatus);
+      });
+      core.on("state", (state) => {
+        setState(state);
+      });
+      core.on("stderr", (stderr) => {
+        setStderr((lines) => {
+          lines.push(stderr);
+          return lines;
+        });
+      });
+    },
+    stop: () => {
+      if (abortControllerRef.current != null) {
+        abortControllerRef.current.abort();
+      }
+    },
+    state,
+    stderr,
+    exitStatus,
+  };
 }
