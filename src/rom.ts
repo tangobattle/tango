d52a027bab65aed489cbe4bcf58d9ba8be826803
bigfarts <bigfarts@punymail.com>
2022-04-22 13:30:43 -0700

    add known rom info

--- src/rom.ts
+++ src/rom.ts
@@ -11,3 +11,15 @@ export function getROMInfo(buffer: ArrayBuffer) {
   const title = decoder.decode(new Uint8Array(buffer, 0x000000a0, 12));
   return { title, crc32: crc32.buf(new Uint8Array(buffer)) >>> 0 };
 }
+
+export interface KnownROM {
+  crc32: number;
+  netplayCompatiblity: string;
+}
+
+export const KNOWN_ROMS: { [title: string]: KnownROM } = {
+  MEGAMAN6_FXX: { crc32: 0xdee6f2a9, netplayCompatiblity: "bn6f" },
+  MEGAMAN6_GXX: { crc32: 0x79452182, netplayCompatiblity: "bn6g" },
+  ROCKEXE6_RXX: { crc32: 0x2dfb603e, netplayCompatiblity: "exe6f" },
+  ROCKEXE6_GXX: { crc32: 0x6285918a, netplayCompatiblity: "exe6g" },
+};
