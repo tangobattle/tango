c02fd0287d17c0bdc5590450ea3c0c0257a1f0d1
bigfarts <bigfarts@punymail.com>
2022-04-21 18:11:04 -0700

    add hook

--- src/ipc/index.ts
+++ src/ipc/index.ts
@@ -2,14 +2,21 @@ import { ChildProcessWithoutNullStreams, spawn } from "child_process";
 import * as types from "./types";
 import { app } from "@electron/remote";
 import path from "path";
+import { EventEmitter } from "stream";
 
-export class Core {
+export interface ExitStatus {
+  exitCode: number | null;
+  signalCode: NodeJS.Signals | null;
+}
+
+export class Core extends EventEmitter {
   private _proc: ChildProcessWithoutNullStreams;
-  private _stderr: string[];
   private _exitPromise: Promise<void>;
 
   constructor(args: types.Args, { signal }: { signal?: AbortSignal } = {}) {
-    const proc = spawn(
+    super();
+
+    this._proc = spawn(
       path.join(app.getAppPath(), "core", "tango-core"),
       [JSON.stringify(args)],
       {
@@ -17,46 +24,50 @@ export class Core {
       }
     );
     this._exitPromise = new Promise((resolve) => {
-      proc.addListener("exit", () => {
-        proc.kill();
+      this._proc.addListener("exit", () => {
+        this._proc.kill();
         resolve();
       });
     });
     window.addEventListener("beforeunload", () => {
-      proc.kill();
+      this._proc.kill();
     });
 
-    const stderr = [] as string[];
-
     (async () => {
-      for await (const data of proc.stderr) {
-        stderr.push(data);
+      for await (const data of this!._proc.stderr) {
+        this!.emit("stderr", data);
       }
     })();
 
-    this._proc = proc;
-    this._stderr = stderr;
-  }
+    (async () => {
+      let buf = "";
+      for await (const data of this!._proc.stdout) {
+        buf += data;
+        const lines = buf.split(/\n/g);
+        buf = lines[lines.length - 1];
 
-  public async *readEventStream() {
-    let buf = "";
-    for await (const data of this._proc.stdout) {
-      buf += data;
-      const lines = buf.split(/\n/g);
-      buf = lines[lines.length - 1];
-
-      for (const r of lines.slice(0, -1)) {
-        yield JSON.parse(r) as types.Notification;
+        for (const r of lines.slice(0, -1)) {
+          const notification = JSON.parse(r) as types.Notification;
+          if (notification.State) {
+            this!.emit("state", notification.State);
+          }
+        }
       }
-    }
-  }
 
-  public stderr() {
-    return this._stderr.join("");
-  }
-
-  public async wait() {
-    await this._exitPromise;
-    return { exitCode: this._proc.exitCode, signalCode: this._proc.signalCode };
+      await this!._exitPromise;
+      this!.emit("exit", {
+        exitCode: this!._proc.exitCode,
+        signalCode: this!._proc.signalCode,
+      });
+    })();
   }
 }
+
+export declare interface Core {
+  on(event: "state", listener: (state: types.State) => void): this;
+  on(event: "stderr", listener: (stderr: string) => void): this;
+  on(event: "exit", listener: (exitStatus: ExitStatus) => void): this;
+}
+
+export type Args = types.Args;
+export type State = types.State;
--- src/renderer/components/App.tsx
+++ src/renderer/components/App.tsx
@@ -1,6 +1,6 @@
-import { Core } from "../../ipc";
+import * as ipc from "../../ipc";
 import { Box, CssBaseline, ThemeProvider } from "@mui/material";
-import React from "react";
+import React, { useEffect } from "react";
 import theme from "../theme";
 
 const matchmakingConnectAddr = "wss://mm.tango.murk.land";
@@ -13,44 +13,70 @@ const iceServers = [
   "stun://stun4.l.google.com:19302",
 ];
 
-export default function App(): JSX.Element {
-  React.useEffect(() => {
-    (async () => {
-      const core = new Core({
-        rom_path: "roms/exe6f.gba",
-        save_path: "roms/exe6f.sav",
-        session_id: "zz",
-        match_type: 0,
-        replay_prefix: "replay",
-        matchmaking_connect_addr: matchmakingConnectAddr,
-        ice_servers: iceServers,
-        input_delay: 3,
-        keymapping: {
-          up: "Up",
-          down: "Down",
-          left: "Left",
-          right: "Right",
-          a: "Z",
-          b: "X",
-          l: "A",
-          r: "S",
-          select: "Back",
-          start: "Return",
-        },
-      });
+function useCore(args: ipc.Args) {
+  const [state, setState] = React.useState<ipc.State | null>(null);
+  const [stderr, setStderr] = React.useState<string[]>([]);
+  const [exitStatus, setExitStatus] = React.useState<ipc.ExitStatus | null>(
+    null
+  );
 
-      for await (const event of core.readEventStream()) {
-        console.log(event);
-      }
-      console.log(await core.wait());
-      console.log(core.stderr());
-    })();
+  useEffect(() => {
+    const abortController = new AbortController();
+    const core = new ipc.Core(args, {
+      signal: abortController.signal,
+    });
+    core.on("exit", (exitStatus) => {
+      setExitStatus(exitStatus);
+    });
+    core.on("state", (state) => {
+      setState(state);
+    });
+    core.on("stderr", (stderr) => {
+      setStderr((lines) => {
+        lines.push(stderr);
+        return lines;
+      });
+    });
+    return () => {
+      abortController.abort();
+    };
+  }, []);
+
+  return { state, stderr, exitStatus };
+}
+
+export default function App(): JSX.Element {
+  const core = useCore({
+    rom_path: "roms/exe6f.gba",
+    save_path: "roms/exe6f.sav",
+    session_id: "zz",
+    match_type: 0,
+    replay_prefix: "replay",
+    matchmaking_connect_addr: matchmakingConnectAddr,
+    ice_servers: iceServers,
+    input_delay: 3,
+    keymapping: {
+      up: "Up",
+      down: "Down",
+      left: "Left",
+      right: "Right",
+      a: "Z",
+      b: "X",
+      l: "A",
+      r: "S",
+      select: "Back",
+      start: "Return",
+    },
   });
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Box>
-        <main>henlo</main>
+        <main>
+          <div>{core.state}</div>
+          <pre>{core.stderr.join("")}</pre>
+        </main>
       </Box>
     </ThemeProvider>
   );
