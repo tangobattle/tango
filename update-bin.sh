b8eb7651a6c5ccdcc6159ea0998c68f3a8ea2fc3
bigfarts <bigfarts@punymail.com>
2022-04-26 10:31:24 -0700

    add ffmpeg

--- update-bin.sh
+++ update-bin.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 set -euo pipefail
 set -x
+
 TANGO_CORE_VERSION="3.0.0-alpha.1"
 TANGO_CORE_PLATFORM="x86_64-pc-windows-gnu"
-curl -L -o tango-core.zip "https://github.com/tangobattle/tango-core/releases/download/v${TANGO_CORE_VERSION}/tango-core-v${TANGO_CORE_VERSION}-${TANGO_CORE_PLATFORM}.zip"
+FFMPEG_VERSION="2022-04-21-git-83e1a1de88"
+
+tempdir="$(mktemp -d)"
+trap 'rm -rf -- "$tempdir"' EXIT
+
+curl -L -o "${tempdir}/tango-core.zip" "https://github.com/tangobattle/tango-core/releases/download/v${TANGO_CORE_VERSION}/tango-core-v${TANGO_CORE_VERSION}-${TANGO_CORE_PLATFORM}.zip"
 rm -rf bin
 mkdir bin || true
-cd bin
-unzip ../tango-core.zip
+pushd bin
+unzip "${tempdir}/tango-core.zip"
+popd
+
+curl -L -o "${tempdir}/ffmpeg.7z" https://www.gyan.dev/ffmpeg/builds/packages/ffmpeg-${FFMPEG_VERSION}-essentials_build.7z
+mkdir "${tempdir}/ffmpeg"
+pushd "${tempdir}/ffmpeg"
+7z x ../ffmpeg.7z
+popd
+
+cp "${tempdir}/ffmpeg/ffmpeg-${FFMPEG_VERSION}-essentials_build/bin/ffmpeg.exe" bin
