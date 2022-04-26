e92cede8a8a12a97f2f0f436802cd96b08892713
bigfarts <bigfarts@punymail.com>
2022-04-25 18:57:05 -0700

    add replay scanning

--- src/renderer/components/panes/ReplaysPane.tsx
+++ src/renderer/components/panes/ReplaysPane.tsx
@@ -1,7 +1,10 @@
+import { opendir } from "fs/promises";
+import path from "path";
 import React from "react";
 import AutoSizer from "react-virtualized-auto-sizer";
 import { FixedSizeList, ListChildComponentProps } from "react-window";
 
+import FolderIcon from "@mui/icons-material/Folder";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import VideoFileIcon from "@mui/icons-material/VideoFile";
 import Box from "@mui/material/Box";
@@ -10,17 +13,37 @@ import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import Stack from "@mui/material/Stack";
 
+import { getReplaysPath } from "../../../paths";
+
+async function* walk(dir: string, root?: string): AsyncIterable<string> {
+  if (root == null) {
+    root = dir;
+  }
+  for await (const d of await opendir(dir)) {
+    const entry = path.join(dir, d.name);
+    if (d.isDirectory()) {
+      yield* await walk(entry, root);
+    } else if (d.isFile()) {
+      yield path.relative(root, entry);
+    }
+  }
+}
+
 function ReplayItem({ index, style }: ListChildComponentProps) {
   return (
     <ListItem
       style={style}
       key={index}
+      sx={{ userSelect: "none" }}
       secondaryAction={
-        <Stack spacing={1} direction="row">
-          <IconButton edge="end">
+        <Stack direction="row">
+          <IconButton>
+            <FolderIcon />
+          </IconButton>
+          <IconButton>
             <VideoFileIcon />
           </IconButton>
-          <IconButton edge="end">
+          <IconButton>
             <PlayArrowIcon />
           </IconButton>
         </Stack>
@@ -32,6 +55,14 @@ function ReplayItem({ index, style }: ListChildComponentProps) {
 }
 
 export default function ReplaysPane({ active }: { active: boolean }) {
+  React.useEffect(() => {
+    (async () => {
+      for await (const entry of walk(getReplaysPath())) {
+        console.log(entry);
+      }
+    })();
+  }, [active]);
+
   return (
     <Box
       sx={{
