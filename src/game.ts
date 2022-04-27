f270b94a24e9248b4cb53793f2bfd19cd3cfe2a0
bigfarts <bigfarts@punymail.com>
2022-04-26 21:24:03 -0700

    use buffer directly don't pull out underlying buffer

--- src/game.ts
+++ src/game.ts
@@ -5,11 +5,11 @@ import tmp from "tmp-promise";
 import patchers from "./patch";
 
 export async function makeROM(romPath: string, patchPath: string | null) {
-  let rom = new Uint8Array((await readFile(romPath)).buffer);
+  let rom = new Uint8Array(await readFile(romPath));
   if (patchPath != null) {
     rom = patchers[path.extname(patchPath).slice(1) as "ips" | "bps"](
       rom,
-      new Uint8Array((await readFile(patchPath)).buffer)
+      new Uint8Array(await readFile(patchPath))
     );
   }
 
