5239da05dcf763c69e437c0870c2d7b7a6d1cee3
bigfarts <bigfarts@punymail.com>
2022-04-27 02:00:46 -0700

    fixes #7

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,10 +1,9 @@
-import "./i18n";
-
 import * as log from "electron-log";
 import React from "react";
 import { render } from "react-dom";
 
 import App from "./components/App";
+import i18n from "./i18n";
 
 Object.assign(console, log.functions);
 
@@ -12,5 +11,9 @@ const root = document.createElement("div");
 
 root.id = "root";
 document.body.appendChild(root);
+
+i18n.on("languageChanged", (lng) => {
+  document.documentElement.setAttribute("lang", lng);
+});
 
 render(<App />, document.getElementById("root"));
