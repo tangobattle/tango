ecf0d859ed16066589affb097e66b14180b4a790
bigfarts <bigfarts@punymail.com>
2022-04-29 22:04:59 -0700

    bundle drmingw

--- update-bin.sh
+++ update-bin.sh
@@ -20,5 +20,11 @@ mkdir "${tempdir}/ffmpeg"
 pushd "${tempdir}/ffmpeg"
 7z x ../ffmpeg.7z
 popd
-
 cp "${tempdir}"/ffmpeg/ffmpeg-*-essentials_build/bin/ffmpeg.exe bin
+
+curl -L -o "${tempdir}/drmingw.7z" "https://github.com/jrfonseca/drmingw/releases/download/0.9.5/drmingw-0.9.5-win64.7z"
+mkdir "${tempdir}/drmingw"
+pushd "${tempdir}/drmingw"
+7z x ../drmingw.7z
+popd
+cp "${tempdir}"/drmingw/drmingw-*-win64/bin/* bin
