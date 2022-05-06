4ac543511ec2262a2f1c103ed25c17492e41d657
bigfarts <bigfarts@punymail.com>
2022-05-05 18:20:57 -0700

    more UI

--- src/renderer/components/panes/BattlePane.tsx
+++ src/renderer/components/panes/BattlePane.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import { Trans } from "react-i18next";
 
 import AddIcon from "@mui/icons-material/Add";
+import SportsMmaOutlinedIcon from "@mui/icons-material/SportsMmaOutlined";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
 
 export default function BattlePane({ active }: { active: boolean }) {
+  const lobbies = [""];
+
   return (
     <Box
       sx={{
@@ -16,23 +20,58 @@ export default function BattlePane({ active }: { active: boolean }) {
         display: active ? "flex" : "none",
       }}
     >
-      <Stack sx={{ flexGrow: 1, width: 0 }} spacing={1}>
-        <Box flexGrow={1} sx={{ px: 1 }}></Box>
-
-        <Stack sx={{ px: 1 }}>
-          <Box sx={{ alignSelf: "flex-end" }}>
-            <Button
-              color="primary"
-              size="medium"
-              variant="contained"
-              onClick={() => {}}
-              startIcon={<AddIcon />}
+      <Box flexGrow={1}>
+        <Box
+          sx={{
+            width: "100%",
+            height: "100%",
+            display: active ? "flex" : "none",
+          }}
+        >
+          {lobbies.length > 0 ? (
+            <Stack sx={{ flexGrow: 1, width: 0 }} spacing={1}>
+              <Box flexGrow={1} flexShrink={0}></Box>
+              <Stack sx={{ px: 1 }}>
+                <Box sx={{ alignSelf: "flex-end" }}>
+                  <Button
+                    color="primary"
+                    size="medium"
+                    variant="contained"
+                    onClick={() => {}}
+                    startIcon={<AddIcon />}
+                  >
+                    <Trans i18nKey="battle:create-lobby" />
+                  </Button>
+                </Box>
+              </Stack>
+            </Stack>
+          ) : (
+            <Box
+              flexGrow={1}
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              sx={{ userSelect: "none", color: "text.disabled" }}
             >
-              <Trans i18nKey="battle:create-lobby" />
-            </Button>
-          </Box>
-        </Stack>
-      </Stack>
+              <Stack alignItems="center" spacing={1}>
+                <SportsMmaOutlinedIcon sx={{ fontSize: "4rem" }} />
+                <Typography variant="h6">
+                  <Trans i18nKey="battle:no-lobbies" />
+                </Typography>
+                <Button
+                  color="primary"
+                  size="medium"
+                  variant="contained"
+                  onClick={() => {}}
+                  startIcon={<AddIcon />}
+                >
+                  <Trans i18nKey="battle:placeholder-create-lobby" />
+                </Button>
+              </Stack>
+            </Box>
+          )}
+        </Box>
+      </Box>
     </Box>
   );
 }
