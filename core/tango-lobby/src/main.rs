08b40ec3862ad6c3b0826e228e0837f6e88791af
bigfarts <bigfarts@punymail.com>
2022-05-06 00:13:26 -0700

    add signaling handler to lobby

--- core/tango-lobby/src/main.rs
+++ core/tango-lobby/src/main.rs
@@ -1,5 +1,6 @@
 mod signaling;
 use envconfig::Envconfig;
+use routerify::ext::RequestExt;
 
 #[derive(Envconfig)]
 struct Config {
@@ -7,20 +8,54 @@ struct Config {
     pub listen_addr: String,
 }
 
+struct State {
+    signaling_server: std::sync::Arc<signaling::Server>,
+}
+
+async fn handle_signaling_request(
+    mut request: hyper::Request<hyper::Body>,
+) -> Result<hyper::Response<hyper::Body>, anyhow::Error> {
+    if !hyper_tungstenite::is_upgrade_request(&request) {
+        return Ok(hyper::Response::builder()
+            .status(hyper::StatusCode::BAD_REQUEST)
+            .body(hyper::Body::empty())?);
+    }
+
+    let (response, websocket) = hyper_tungstenite::upgrade(&mut request, None)?;
+
+    // Spawn a task to handle the websocket connection.
+    let signaling_server = request.data::<State>().unwrap().signaling_server.clone();
+    tokio::spawn(async move {
+        if let Err(e) = signaling_server.handle_connection(websocket).await {
+            eprintln!("Error in websocket connection: {}", e);
+        }
+    });
+
+    // Return the response so the spawned future can continue.
+    Ok(response)
+}
+
+fn router() -> routerify::Router<hyper::Body, anyhow::Error> {
+    routerify::Router::builder()
+        .data(State {
+            signaling_server: std::sync::Arc::new(signaling::Server::new()),
+        })
+        .get("/signaling", handle_signaling_request)
+        .build()
+        .unwrap()
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let router = router();
+
     env_logger::Builder::from_default_env()
-        .filter(Some("tango"), log::LevelFilter::Info)
-        .filter(Some("signaling_server"), log::LevelFilter::Info)
+        .filter(Some("tango-lobby"), log::LevelFilter::Info)
         .init();
-    log::info!(
-        "welcome to tango's signaling_server v{}-{}!",
-        env!("CARGO_PKG_VERSION"),
-        git_version::git_version!()
-    );
+    log::info!("welcome to tango-lobby {}!", git_version::git_version!());
     let config = Config::init_from_env().unwrap();
-    let listener = tokio::net::TcpListener::bind(config.listen_addr).await?;
-    let mut server = signaling::Server::new(listener);
-    server.run().await;
+    let addr = config.listen_addr.parse()?;
+    let service = routerify::RouterService::new(router).unwrap();
+    hyper::Server::bind(&addr).serve(service).await?;
     Ok(())
 }
--- core/tango-lobby/src/signaling.rs
+++ core/tango-lobby/src/signaling.rs
@@ -6,187 +6,177 @@ struct Session {
     offer_sdp: String,
     sinks: Vec<
         futures_util::stream::SplitSink<
-            tokio_tungstenite::WebSocketStream<tokio::net::TcpStream>,
+            hyper_tungstenite::WebSocketStream<hyper::upgrade::Upgraded>,
             tokio_tungstenite::tungstenite::Message,
         >,
     >,
 }
 
 pub struct Server {
-    listener: tokio::net::TcpListener,
     sessions: std::sync::Arc<
         tokio::sync::Mutex<
             std::collections::HashMap<String, std::sync::Arc<tokio::sync::Mutex<Session>>>,
         >,
     >,
-}
-
-async fn handle_connection(
-    sessions: std::sync::Arc<
-        tokio::sync::Mutex<
-            std::collections::HashMap<String, std::sync::Arc<tokio::sync::Mutex<Session>>>,
-        >,
-    >,
-    raw_stream: tokio::net::TcpStream,
-    addr: std::net::SocketAddr,
-) -> anyhow::Result<()> {
-    let (tx, mut rx) = tokio_tungstenite::accept_async(raw_stream).await?.split();
-    let mut tx = Some(tx);
-    let session_id = std::sync::Arc::new(tokio::sync::Mutex::new(None));
-    let mut session = None;
-    let mut me: usize = 0;
-
-    let r = {
-        let sessions = sessions.clone();
-        let session_id = session_id.clone();
-        (move || async move {
-            loop {
-                let msg = match rx.try_next().await? {
-                    Some(tokio_tungstenite::tungstenite::Message::Binary(d)) => {
-                        tango_protos::signaling::Packet::decode(bytes::Bytes::from(d))?
-                    }
-                    Some(tokio_tungstenite::tungstenite::Message::Close(_)) | None => {
-                        break;
-                    }
-                    Some(m) => {
-                        anyhow::bail!("unexpected message: {:?}", m);
-                    }
-                };
-                log::debug!("received message from {}: {:?}", addr, msg);
-                match msg.which {
-                    Some(tango_protos::signaling::packet::Which::Start(start)) => {
-                        let mut sessions = sessions.lock().await;
-                        session = Some(
-                            sessions
-                                .entry(start.session_id.clone())
-                                .or_insert_with(|| {
-                                    std::sync::Arc::new(tokio::sync::Mutex::new(Session {
-                                        num_clients: 0,
-                                        offer_sdp: start.offer_sdp.clone(),
-                                        sinks: vec![],
-                                    }))
-                                })
-                                .clone(),
-                        );
-
-                        let session = session.as_ref().unwrap();
-                        let mut session = session.lock().await;
-                        session.num_clients += 1;
-                        *session_id.lock().await = Some(start.session_id.clone());
-                        let offer_sdp = session.offer_sdp.to_string();
-
-                        me = session.sinks.len();
-                        session.sinks.push(tx.take().unwrap());
-
-                        if me == 1 {
-                            session.sinks[me]
-                                .send(tokio_tungstenite::tungstenite::Message::Binary(
-                                    tango_protos::signaling::Packet {
-                                        which: Some(tango_protos::signaling::packet::Which::Offer(
-                                            tango_protos::signaling::packet::Offer {
-                                                sdp: offer_sdp,
-                                            },
-                                        )),
-                                    }
-                                    .encode_to_vec(),
-                                ))
-                                .await?;
-                        }
-                    }
-                    Some(tango_protos::signaling::packet::Which::Offer(_)) => {
-                        anyhow::bail!(
-                            "received offer from client: only the server may send offers"
-                        );
-                    }
-                    Some(tango_protos::signaling::packet::Which::Answer(answer)) => {
-                        let session = match session.as_ref() {
-                            Some(session) => session,
-                            None => {
-                                anyhow::bail!("no session active");
-                            }
-                        };
-                        let mut session = session.lock().await;
-                        session.sinks[0]
-                            .send(tokio_tungstenite::tungstenite::Message::Binary(
-                                tango_protos::signaling::Packet {
-                                    which: Some(tango_protos::signaling::packet::Which::Answer(
-                                        tango_protos::signaling::packet::Answer { sdp: answer.sdp },
-                                    )),
-                                }
-                                .encode_to_vec(),
-                            ))
-                            .await?;
-                    }
-                    Some(tango_protos::signaling::packet::Which::IceCandidate(ice_candidate)) => {
-                        let session = match session.as_ref() {
-                            Some(session) => session,
-                            None => {
-                                anyhow::bail!("no session active");
-                            }
-                        };
-                        let mut session = session.lock().await;
-                        session.sinks[1 - me]
-                            .send(tokio_tungstenite::tungstenite::Message::Binary(
-                                tango_protos::signaling::Packet {
-                                    which: Some(
-                                        tango_protos::signaling::packet::Which::IceCandidate(
-                                            tango_protos::signaling::packet::IceCandidate {
-                                                candidate: ice_candidate.candidate,
-                                                mid: ice_candidate.mid,
-                                            },
-                                        ),
-                                    ),
-                                }
-                                .encode_to_vec(),
-                            ))
-                            .await?;
-                    }
-                    p => anyhow::bail!("unknown packet: {:?}", p),
-                }
-            }
-            Ok(())
-        })()
-        .await
-    };
-
-    if let Some(session_id) = &*session_id.lock().await {
-        let mut sessions = sessions.lock().await;
-        let should_delete = {
-            if let Some(session) = sessions.get(session_id) {
-                let mut session = session.lock().await;
-                session.num_clients -= 1;
-                true
-            } else {
-                false
-            }
-        };
-
-        if should_delete {
-            sessions.remove(session_id);
-        }
-    }
-
-    r
 }
 
 impl Server {
-    pub fn new(listener: tokio::net::TcpListener) -> Server {
+    pub fn new() -> Server {
         Server {
-            listener,
             sessions: std::sync::Arc::new(
                 tokio::sync::Mutex::new(std::collections::HashMap::new()),
             ),
         }
     }
 
-    pub async fn run(&mut self) {
-        while let Ok((stream, addr)) = self.listener.accept().await {
+    pub async fn handle_connection(
+        &self,
+        ws: hyper_tungstenite::HyperWebsocket,
+    ) -> anyhow::Result<()> {
+        let (tx, mut rx) = ws.await?.split();
+        let mut tx = Some(tx);
+        let session_id = std::sync::Arc::new(tokio::sync::Mutex::new(None));
+        let mut session = None;
+        let mut me: usize = 0;
+
+        let r = {
             let sessions = self.sessions.clone();
-            tokio::spawn(async move {
-                if let Err(e) = handle_connection(sessions, stream, addr).await {
-                    log::warn!("client {} disconnected with error: {}", addr, e);
+            let session_id = session_id.clone();
+            (move || async move {
+                loop {
+                    let msg = match rx.try_next().await? {
+                        Some(tokio_tungstenite::tungstenite::Message::Binary(d)) => {
+                            tango_protos::signaling::Packet::decode(bytes::Bytes::from(d))?
+                        }
+                        Some(tokio_tungstenite::tungstenite::Message::Close(_)) | None => {
+                            break;
+                        }
+                        Some(m) => {
+                            anyhow::bail!("unexpected message: {:?}", m);
+                        }
+                    };
+                    log::debug!("received message: {:?}", msg);
+                    match msg.which {
+                        Some(tango_protos::signaling::packet::Which::Start(start)) => {
+                            let mut sessions = sessions.lock().await;
+                            session = Some(
+                                sessions
+                                    .entry(start.session_id.clone())
+                                    .or_insert_with(|| {
+                                        std::sync::Arc::new(tokio::sync::Mutex::new(Session {
+                                            num_clients: 0,
+                                            offer_sdp: start.offer_sdp.clone(),
+                                            sinks: vec![],
+                                        }))
+                                    })
+                                    .clone(),
+                            );
+
+                            let session = session.as_ref().unwrap();
+                            let mut session = session.lock().await;
+                            session.num_clients += 1;
+                            *session_id.lock().await = Some(start.session_id.clone());
+                            let offer_sdp = session.offer_sdp.to_string();
+
+                            me = session.sinks.len();
+                            session.sinks.push(tx.take().unwrap());
+
+                            if me == 1 {
+                                session.sinks[me]
+                                    .send(tokio_tungstenite::tungstenite::Message::Binary(
+                                        tango_protos::signaling::Packet {
+                                            which: Some(
+                                                tango_protos::signaling::packet::Which::Offer(
+                                                    tango_protos::signaling::packet::Offer {
+                                                        sdp: offer_sdp,
+                                                    },
+                                                ),
+                                            ),
+                                        }
+                                        .encode_to_vec(),
+                                    ))
+                                    .await?;
+                            }
+                        }
+                        Some(tango_protos::signaling::packet::Which::Offer(_)) => {
+                            anyhow::bail!(
+                                "received offer from client: only the server may send offers"
+                            );
+                        }
+                        Some(tango_protos::signaling::packet::Which::Answer(answer)) => {
+                            let session = match session.as_ref() {
+                                Some(session) => session,
+                                None => {
+                                    anyhow::bail!("no session active");
+                                }
+                            };
+                            let mut session = session.lock().await;
+                            session.sinks[0]
+                                .send(tokio_tungstenite::tungstenite::Message::Binary(
+                                    tango_protos::signaling::Packet {
+                                        which: Some(
+                                            tango_protos::signaling::packet::Which::Answer(
+                                                tango_protos::signaling::packet::Answer {
+                                                    sdp: answer.sdp,
+                                                },
+                                            ),
+                                        ),
+                                    }
+                                    .encode_to_vec(),
+                                ))
+                                .await?;
+                        }
+                        Some(tango_protos::signaling::packet::Which::IceCandidate(
+                            ice_candidate,
+                        )) => {
+                            let session = match session.as_ref() {
+                                Some(session) => session,
+                                None => {
+                                    anyhow::bail!("no session active");
+                                }
+                            };
+                            let mut session = session.lock().await;
+                            session.sinks[1 - me]
+                                .send(tokio_tungstenite::tungstenite::Message::Binary(
+                                    tango_protos::signaling::Packet {
+                                        which: Some(
+                                            tango_protos::signaling::packet::Which::IceCandidate(
+                                                tango_protos::signaling::packet::IceCandidate {
+                                                    candidate: ice_candidate.candidate,
+                                                    mid: ice_candidate.mid,
+                                                },
+                                            ),
+                                        ),
+                                    }
+                                    .encode_to_vec(),
+                                ))
+                                .await?;
+                        }
+                        p => anyhow::bail!("unknown packet: {:?}", p),
+                    }
                 }
-            });
+                Ok(())
+            })()
+            .await
+        };
+
+        if let Some(session_id) = &*session_id.lock().await {
+            let mut sessions = self.sessions.lock().await;
+            let should_delete = {
+                if let Some(session) = sessions.get(session_id) {
+                    let mut session = session.lock().await;
+                    session.num_clients -= 1;
+                    true
+                } else {
+                    false
+                }
+            };
+
+            if should_delete {
+                sessions.remove(session_id);
+            }
         }
+
+        r
     }
 }
