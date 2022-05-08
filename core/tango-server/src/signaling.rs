7f1f5c3beaedbd7af81b9813d092d7cbd0122a0f
bigfarts <bigfarts@punymail.com>
2022-05-08 00:27:12 -0700

    simplify signaling server

--- core/tango-server/src/signaling.rs
+++ core/tango-server/src/signaling.rs
@@ -2,7 +2,6 @@ use futures_util::{SinkExt, StreamExt, TryStreamExt};
 use prost::Message;
 
 struct Session {
-    num_clients: usize,
     offer_sdp: String,
     sinks: Vec<
         futures_util::stream::SplitSink<
@@ -34,15 +33,16 @@ impl Server {
         ws: hyper_tungstenite::WebSocketStream<hyper::upgrade::Upgraded>,
     ) -> anyhow::Result<()> {
         let (tx, mut rx) = ws.split();
-        let mut tx = Some(tx);
-        let session_id = std::sync::Arc::new(tokio::sync::Mutex::new(None));
-        let mut session = None;
-        let mut me: usize = 0;
+        let session_id_for_cleanup = std::sync::Arc::new(tokio::sync::Mutex::new(None));
 
         let r = {
             let sessions = self.sessions.clone();
-            let session_id = session_id.clone();
+            let session_id_for_cleanup = session_id_for_cleanup.clone();
             (move || async move {
+                let mut session = None;
+                let mut tx = Some(tx);
+                let mut me: usize = 0;
+
                 loop {
                     let msg = match rx.try_next().await? {
                         Some(tungstenite::Message::Binary(d)) => {
@@ -59,18 +59,20 @@ impl Server {
                     match msg.which {
                         Some(tango_protos::signaling::packet::Which::Start(start)) => {
                             let mut sessions = sessions.lock().await;
-                            session = Some(
-                                sessions
-                                    .entry(start.session_id.clone())
-                                    .or_insert_with(|| {
-                                        std::sync::Arc::new(tokio::sync::Mutex::new(Session {
-                                            num_clients: 0,
-                                            offer_sdp: start.offer_sdp.clone(),
-                                            sinks: vec![],
-                                        }))
-                                    })
-                                    .clone(),
-                            );
+                            session =
+                                Some(if let Some(session) = sessions.remove(&start.session_id) {
+                                    session
+                                } else {
+                                    sessions
+                                        .entry(start.session_id.clone())
+                                        .or_insert_with(|| {
+                                            std::sync::Arc::new(tokio::sync::Mutex::new(Session {
+                                                offer_sdp: start.offer_sdp.clone(),
+                                                sinks: vec![],
+                                            }))
+                                        })
+                                        .clone()
+                                });
 
                             let session = if let Some(session) = session.as_ref() {
                                 session
@@ -78,8 +80,7 @@ impl Server {
                                 anyhow::bail!("no such session");
                             };
                             let mut session = session.lock().await;
-                            session.num_clients += 1;
-                            *session_id.lock().await = Some(start.session_id.clone());
+                            *session_id_for_cleanup.lock().await = Some(start.session_id.clone());
                             let offer_sdp = session.offer_sdp.to_string();
 
                             me = session.sinks.len();
@@ -169,21 +170,9 @@ impl Server {
             .await
         };
 
-        if let Some(session_id) = &*session_id.lock().await {
+        if let Some(session_id) = &*session_id_for_cleanup.lock().await {
             let mut sessions = self.sessions.lock().await;
-            let should_delete = {
-                if let Some(session) = sessions.get(session_id) {
-                    let mut session = session.lock().await;
-                    session.num_clients -= 1;
-                    true
-                } else {
-                    false
-                }
-            };
-
-            if should_delete {
-                sessions.remove(session_id);
-            }
+            sessions.remove(session_id);
         }
 
         r
