23a9f9bb3b1ed4b456e245c9b85537c05734fb72
bigfarts <bigfarts@punymail.com>
2022-06-20 16:34:53 -0700

    hide overflow in replays pane

--- launcher/src/renderer/components/panes/ReplaysPane.tsx
+++ launcher/src/renderer/components/panes/ReplaysPane.tsx
@@ -253,6 +253,7 @@ export default function ReplaysPane({ active }: { active: boolean }) {
       sx={{
         width: "100%",
         height: "100%",
+        overflow: "hidden",
         display: active ? "flex" : "none",
       }}
     >
