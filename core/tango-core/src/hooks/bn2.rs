aeb14217b401bea4e97213725ce9ff6a0a90b43d
bigfarts <bigfarts@punymail.com>
2022-06-29 02:13:27 -0700

    add random background

--- core/tango-core/src/hooks/bn2.rs
+++ core/tango-core/src/hooks/bn2.rs
@@ -28,7 +28,7 @@ impl BN2 {
 }
 
 fn random_background(rng: &mut impl rand::Rng) -> u8 {
-    const BATTLE_BACKGROUNDS: &[u8] = &[0x00]; // TODO
+    const BATTLE_BACKGROUNDS: &[u8] = &[0x00, 0x01, 0x02, 0x03, 0x05, 0x08, 0x15, 0x18];
     BATTLE_BACKGROUNDS[rng.gen_range(0..BATTLE_BACKGROUNDS.len())]
 }
 
