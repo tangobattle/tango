8cdd7768280f2a4e596478eb02613bf51fbb7c3c
bigfarts <bigfarts@punymail.com>
2022-07-20 20:13:22 -0700

    add BN4 offsets

--- launcher/src/saveedit/bn4/index.ts
+++ launcher/src/saveedit/bn4/index.ts
@@ -181,7 +181,7 @@ class FolderEditor {
   }
 
   getElementIcons() {
-    return [];
+    return this.editor.romViewer.getElementIcons();
   }
 }
 
@@ -327,11 +327,14 @@ interface SaveeditInfo {
     chipData: number;
     chipIconPalettePointer: number;
     chipNamesPointers: number;
+    elementIconPalettePointer: number;
+    elementIconsPointer: number;
   };
 }
 
 class ROMViewer extends ROMViewerBase {
-  private palette: Uint32Array;
+  private chipIconPalette: Uint32Array;
+  private elementIconPalette: Uint32Array;
   private saveDv: DataView;
   private saveeditInfo: SaveeditInfo;
 
@@ -343,13 +346,31 @@ class ROMViewer extends ROMViewerBase {
     super(buffer);
     this.saveDv = saveDv;
     this.saveeditInfo = saveeditInfo;
-    this.palette = getPalette(
+    this.chipIconPalette = getPalette(
       this.dv,
       this.dv.getUint32(
         this.saveeditInfo.offsets.chipIconPalettePointer,
         true
       ) & ~0x08000000
     );
+    this.elementIconPalette = getPalette(
+      this.dv,
+      this.dv.getUint32(saveeditInfo.offsets.elementIconPalettePointer, true) &
+        ~0x08000000
+    );
+  }
+
+  getElementIcons(): ImageData[] {
+    const icons: ImageData[] = [];
+    const start =
+      this.dv.getUint32(this.saveeditInfo.offsets.elementIconsPointer, true) &
+      ~0x08000000;
+    for (let i = 0; i < 13; ++i) {
+      icons.push(
+        getChipIcon(this.dv, this.elementIconPalette, start + i * 0x80)
+      );
+    }
+    return icons;
   }
 
   getChipInfo(id: number): Chip {
@@ -376,8 +397,12 @@ class ROMViewer extends ROMViewerBase {
       codes: codes.join(""),
       icon:
         iconPtr >= 0x08000000
-          ? getChipIcon(this.dv, this.palette, iconPtr & ~0x08000000)
-          : getChipIcon(this.saveDv, this.palette, iconPtr & ~0x02000000),
+          ? getChipIcon(this.dv, this.chipIconPalette, iconPtr & ~0x08000000)
+          : getChipIcon(
+              this.saveDv,
+              this.chipIconPalette,
+              iconPtr & ~0x02000000
+            ),
       element: this.dv.getUint8(dataOffset + 0x07),
       class: ["standard", "mega", "giga"][this.dv.getUint8(dataOffset + 0x08)],
       mb: this.dv.getUint8(dataOffset + 0x06),
