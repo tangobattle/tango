dec68abbe8a5b50a467e457fdf658f1671487341
bigfarts <bigfarts@punymail.com>
2022-08-29 14:31:30 -0700

    allow macos make_app from anywhere

--- macos/make_app.sh
+++ macos/make_app.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 set -euo pipefail
+
+# Cleanup.
 rm -rf Tango.app
+
+# Create directory structure.
 mkdir Tango.app{,/Contents{,/{MacOS,Resources}}}
-./generate_info_plist.py >Tango.app/Contents/Info.plist
+
+# Generate an appropriate Info.plist.
+"$(dirname "${BASH_SOURCE[0]}")/generate_info_plist.py" >Tango.app/Contents/Info.plist
+
+# Build macOS binaries.
+# cargo build --bin tango --target=aarch64-apple-darwin --release
