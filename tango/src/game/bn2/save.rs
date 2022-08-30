c07825138ed9c069470b68f94b89716df0e6fc6a
zachristmas <zachristmas@icloud.com>
2022-08-29 22:53:37 -0400

    Ported ts functions for viewing bn2 battlechips (#94)
    * ported ts functions for viewing bn2 battlechips
    
    * read u16le from the buffer instead of single u8s
    
    * nit: as usize not required here
    
    Co-authored-by: zchristmas <zchristmas.dev@gmail.com>
    Co-authored-by: bigfarts <bigfarts@punymail.com>

--- tango/src/game/bn2/save.rs
+++ tango/src/game/bn2/save.rs
@@ -47,9 +47,61 @@ impl Save {
 }
 
 impl save::Save for Save {
+    fn view_chips(&self) -> Option<Box<dyn save::ChipsView + '_>> {
+        Some(Box::new(ChipsView { save: self }))
+    }
+
     fn to_vec(&self) -> Vec<u8> {
         let mut buf = vec![0; 65536];
         buf[..SRAM_SIZE].copy_from_slice(&self.buf);
         buf
+    }
+}
+
+pub struct ChipsView<'a> {
+    save: &'a Save,
+}
+
+impl<'a> save::ChipsView<'a> for ChipsView<'a> {
+    fn chip_codes(&self) -> &'static [u8] {
+        &b"ABCDEFGHIJKLMNOPQRSTUVWXYZ*"[..]
+    }
+
+    fn num_folders(&self) -> usize {
+        3
+    }
+
+    fn equipped_folder_index(&self) -> usize {
+        self.save.buf[0x0dc2] as usize
+    }
+
+    fn regular_chip_is_in_place(&self) -> bool {
+        true
+    }
+
+    fn regular_chip_index(&self, folder_index: usize) -> Option<usize> {
+        let idx = self.save.buf[0x0ddd + folder_index];
+        if idx == 0 {
+            None
+        } else {
+            Some(idx as usize)
+        }
+    }
+
+    fn tag_chip_indexes(&self, _folder_index: usize) -> Option<(usize, usize)> {
+        None
+    }
+
+    fn chip(&self, folder_index: usize, chip_index: usize) -> Option<save::Chip> {
+        if folder_index > 3 || chip_index > 30 {
+            return None;
+        }
+
+        let offset = 0x0ab0 + folder_index * (30 * 4) + chip_index * 4;
+
+        Some(save::Chip {
+            id: byteorder::LittleEndian::read_u16(&self.save.buf[offset..offset+2]) as usize,
+            code: byteorder::LittleEndian::read_u16(&self.save.buf[offset+2..offset+4]) as usize,
+        })
     }
 }
