32750372e4536dc43e9d8a0c6d30c4c222d421de
bigfarts <bigfarts@punymail.com>
2022-09-01 15:54:07 -0700

    add better ncp view

--- tango/src/gui/save_view/navicust_view.rs
+++ tango/src/gui/save_view/navicust_view.rs
@@ -10,6 +10,66 @@ impl State {
     }
 }
 
+fn navicust_part_colors(color: &rom::NavicustPartColor) -> (egui::Color32, egui::Color32) {
+    match color {
+        rom::NavicustPartColor::Red => (
+            egui::Color32::from_rgb(0xde, 0x10, 0x00),
+            egui::Color32::from_rgb(0xbd, 0x00, 0x00),
+        ),
+        rom::NavicustPartColor::Pink => (
+            egui::Color32::from_rgb(0xde, 0x8c, 0xc6),
+            egui::Color32::from_rgb(0xbd, 0x6b, 0xa5),
+        ),
+        rom::NavicustPartColor::Yellow => (
+            egui::Color32::from_rgb(0xde, 0xde, 0x00),
+            egui::Color32::from_rgb(0xbd, 0xbd, 0x00),
+        ),
+        rom::NavicustPartColor::Green => (
+            egui::Color32::from_rgb(0x18, 0xc6, 0x00),
+            egui::Color32::from_rgb(0x00, 0xa5, 0x00),
+        ),
+        rom::NavicustPartColor::Blue => (
+            egui::Color32::from_rgb(0x29, 0x84, 0xde),
+            egui::Color32::from_rgb(0x08, 0x60, 0xb8),
+        ),
+        rom::NavicustPartColor::White => (
+            egui::Color32::from_rgb(0xde, 0xde, 0xde),
+            egui::Color32::from_rgb(0xbd, 0xbd, 0xbd),
+        ),
+        rom::NavicustPartColor::Orange => (
+            egui::Color32::from_rgb(0xde, 0x7b, 0x00),
+            egui::Color32::from_rgb(0xde, 0x7b, 0x00),
+        ),
+        rom::NavicustPartColor::Purple => (
+            egui::Color32::from_rgb(0x94, 0x00, 0xce),
+            egui::Color32::from_rgb(0x94, 0x00, 0xce),
+        ),
+        rom::NavicustPartColor::Gray => (
+            egui::Color32::from_rgb(0x84, 0x84, 0x84),
+            egui::Color32::from_rgb(0x84, 0x84, 0x84),
+        ),
+    }
+}
+
+fn show_part_name(
+    ui: &mut egui::Ui,
+    name: egui::RichText,
+    is_enabled: bool,
+    color: &rom::NavicustPartColor,
+) {
+    egui::Frame::none()
+        .inner_margin(egui::style::Margin::symmetric(4.0, 0.0))
+        .rounding(egui::Rounding::same(2.0))
+        .fill(if is_enabled {
+            navicust_part_colors(color).0
+        } else {
+            egui::Color32::from_rgb(0xbd, 0xbd, 0xbd)
+        })
+        .show(ui, |ui| {
+            ui.label(name.color(egui::Color32::BLACK));
+        });
+}
+
 pub struct NavicustView {}
 
 impl NavicustView {
@@ -28,23 +88,43 @@ impl NavicustView {
         assets: &Box<dyn rom::Assets + Send + Sync>,
         _state: &mut State,
     ) {
-        for i in 0..navicust_view.count() {
-            let ncp = if let Some(ncp) = navicust_view.navicust_part(i) {
-                ncp
-            } else {
-                continue;
-            };
+        const NCP_CHIP_WIDTH: f32 = 150.0;
 
-            let info = if let Some(info) = assets.navicust_part(ncp.id, ncp.variant) {
-                info
-            } else {
-                continue;
-            };
+        let items = (0..navicust_view.count())
+            .flat_map(|i| {
+                navicust_view.navicust_part(i).and_then(|ncp| {
+                    assets
+                        .navicust_part(ncp.id, ncp.variant)
+                        .and_then(|info| info.color.as_ref().map(|color| (info, color)))
+                })
+            })
+            .collect::<Vec<_>>();
 
-            ui.label(
-                egui::RichText::new(&info.name)
-                    .family(font_families.for_language(&game.language())),
-            );
-        }
+        ui.horizontal(|ui| {
+            ui.with_layout(egui::Layout::top_down_justified(egui::Align::Min), |ui| {
+                ui.set_width(NCP_CHIP_WIDTH);
+                for (info, color) in items.iter().filter(|(info, _)| info.is_solid) {
+                    show_part_name(
+                        ui,
+                        egui::RichText::new(&info.name)
+                            .family(font_families.for_language(&game.language())),
+                        true,
+                        color,
+                    );
+                }
+            });
+            ui.with_layout(egui::Layout::top_down_justified(egui::Align::Min), |ui| {
+                ui.set_width(NCP_CHIP_WIDTH);
+                for (info, color) in items.iter().filter(|(info, _)| !info.is_solid) {
+                    show_part_name(
+                        ui,
+                        egui::RichText::new(&info.name)
+                            .family(font_families.for_language(&game.language())),
+                        true,
+                        color,
+                    );
+                }
+            });
+        });
     }
 }
