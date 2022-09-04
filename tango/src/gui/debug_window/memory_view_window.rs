de929aacc981974f1892c336c71a7d8a623e31f2
bigfarts <bigfarts@punymail.com>
2022-09-03 18:00:15 -0700

    more memory viewer stuff

--- tango/src/gui/debug_window/memory_view_window.rs
+++ tango/src/gui/debug_window/memory_view_window.rs
@@ -30,42 +30,73 @@ impl MemoryViewWindow {
             .id(egui::Id::new("memory-viewer"))
             .open(&mut open)
             .show(ctx, |ui| {
+                const FONT_WIDTH: f32 = 10.0;
                 let state = state.as_mut().unwrap();
 
+                let mut jumping = false;
                 ui.horizontal(|ui| {
-                    let mut submitted = false;
                     let input_resp = ui.add(
                         egui::TextEdit::singleline(&mut state.jump_to)
-                            .desired_width(8.0 * 12.0)
+                            .desired_width(8.0 * FONT_WIDTH)
                             .hint_text("Jump to")
                             .font(egui::TextStyle::Monospace),
                     );
+                    state.jump_to = state
+                        .jump_to
+                        .chars()
+                        .filter(|c| "0123456789abcdefABCDEF".chars().any(|c2| c2 == *c))
+                        .collect();
                     if input_resp.lost_focus() && ui.ctx().input().key_pressed(egui::Key::Enter) {
-                        submitted = true;
+                        jumping = true;
                     }
 
                     if ui.button("Go!").clicked() {
-                        submitted = true;
-                    }
-
-                    if submitted {
-                        log::info!("Submitted");
+                        jumping = true;
                     }
                 });
-                egui::ScrollArea::vertical()
-                    .auto_shrink([false, false])
-                    .show(ui, |ui| {
-                        let session = session.lock();
-                        let session = if let Some(session) = session.as_ref() {
-                            session
-                        } else {
-                            ui.label("No session in progress.");
-                            return;
-                        };
+                let session = session.lock();
+                let session = if let Some(session) = session.as_ref() {
+                    session
+                } else {
+                    ui.label("No session in progress.");
+                    return;
+                };
 
-                        let thread_handle = session.thread_handle();
-                        let audio_guard = thread_handle.lock_audio();
+                let thread_handle = session.thread_handle();
+                let mut audio_guard = thread_handle.lock_audio();
+
+                const ROW_HEIGHT: f32 = 18.0;
+                let mut sa = egui::ScrollArea::vertical().auto_shrink([true, false]);
+                if jumping {
+                    if let Ok(jump_to) = u32::from_str_radix(&state.jump_to, 16) {
+                        sa = sa.vertical_scroll_offset(
+                            (ROW_HEIGHT + ui.spacing().item_spacing.y) * (jump_to / 0x10) as f32,
+                        );
+                    }
+                }
+
+                sa.show_rows(ui, ROW_HEIGHT, 0x0fffffff / 0x10, |ui, range| {
+                    ui.vertical(|ui| {
+                        for i in range {
+                            ui.horizontal(|ui| {
+                                ui.set_height(ROW_HEIGHT);
+                                let offset = i * 16;
+                                ui.monospace(format!("{:08x}", offset));
+                                let bs = audio_guard
+                                    .core_mut()
+                                    .raw_read_range::<16>(offset as u32, -1);
+                                for b in bs {
+                                    ui.monospace(format!("{:02x}", b));
+                                }
+                                ui.monospace(
+                                    bs.map(|b| if b >= 32 && b < 127 { b as char } else { '.' })
+                                        .iter()
+                                        .collect::<String>(),
+                                );
+                            });
+                        }
                     });
+                });
             });
         if !open {
             *state = None;
