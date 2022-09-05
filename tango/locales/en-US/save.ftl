aab528917ee93d697c1d94a96907a9fcbd2f590c
bigfarts <bigfarts@punymail.com>
2022-09-04 17:44:25 -0700

    add dark soul view

--- tango/locales/en-US/save.ftl
+++ tango/locales/en-US/save.ftl
@@ -5,3 +5,12 @@ save =
     .dark-ai = Dark Soul AI
 
 save-group = Group by chip
+
+dark-ai =
+    .secondary-standard-chips = Standard chips (secondary)
+    .standard-chips = Standard chips
+    .mega-chips = Mega chips
+    .giga-chip = Giga chip
+    .combos = Combos
+    .program-advance = Program advance
+    .unset = Unset
--- tango/src/gui/save_view/dark_ai_view.rs
+++ tango/src/gui/save_view/dark_ai_view.rs
@@ -20,12 +20,12 @@ impl State {
 }
 
 struct MaterializedDarkAI {
-    secondary_standard_chips: Vec<usize>,
-    standard_chips: Vec<usize>,
-    mega_chips: Vec<usize>,
+    secondary_standard_chips: [Option<usize>; 3],
+    standard_chips: [Option<usize>; 16],
+    mega_chips: [Option<usize>; 5],
     giga_chip: Option<usize>,
-    combos: Vec<usize>,
-    pa: Option<usize>,
+    combos: [Option<usize>; 8],
+    program_advance: Option<usize>,
 }
 
 impl MaterializedDarkAI {
@@ -55,67 +55,201 @@ impl MaterializedDarkAI {
             secondary_standard_chips: secondary_use_counts
                 .iter()
                 .enumerate()
-                .filter(|(id, _)| {
+                .filter(|(id, count)| {
                     assets
                         .chip(*id)
                         .map(|c| c.class == rom::ChipClass::Standard)
                         .unwrap_or(false)
+                        && **count > 0
                 })
-                .sorted_by_key(|(_, count)| std::cmp::Reverse(**count))
+                .sorted_by_key(|(id, count)| (std::cmp::Reverse(**count), *id))
+                .map(|(id, _)| Some(id))
+                .chain(std::iter::repeat(None))
                 .take(3)
-                .map(|(id, _)| id)
-                .collect(),
+                .collect::<Vec<_>>()
+                .try_into()
+                .unwrap(),
             standard_chips: use_counts
                 .iter()
                 .enumerate()
-                .filter(|(id, _)| {
+                .filter(|(id, count)| {
                     assets
                         .chip(*id)
                         .map(|c| c.class == rom::ChipClass::Standard)
                         .unwrap_or(false)
+                        && **count > 0
                 })
-                .sorted_by_key(|(_, count)| std::cmp::Reverse(**count))
+                .sorted_by_key(|(id, count)| (std::cmp::Reverse(**count), *id))
+                .map(|(id, _)| Some(id))
+                .chain(std::iter::repeat(None))
                 .take(16)
-                .map(|(id, _)| id)
-                .collect(),
+                .collect::<Vec<_>>()
+                .try_into()
+                .unwrap(),
             mega_chips: use_counts
                 .iter()
                 .enumerate()
-                .filter(|(id, _)| {
+                .filter(|(id, count)| {
                     assets
                         .chip(*id)
                         .map(|c| c.class == rom::ChipClass::Mega)
                         .unwrap_or(false)
+                        && **count > 0
                 })
-                .sorted_by_key(|(_, count)| std::cmp::Reverse(**count))
+                .sorted_by_key(|(id, count)| (std::cmp::Reverse(**count), *id))
+                .map(|(id, _)| Some(id))
+                .chain(std::iter::repeat(None))
                 .take(5)
-                .map(|(id, _)| id)
-                .collect(),
+                .collect::<Vec<_>>()
+                .try_into()
+                .unwrap(),
             giga_chip: use_counts
                 .iter()
                 .enumerate()
-                .filter(|(id, _)| {
+                .filter(|(id, count)| {
                     assets
                         .chip(*id)
                         .map(|c| c.class == rom::ChipClass::Giga)
                         .unwrap_or(false)
+                        && **count > 0
                 })
-                .max_by_key(|(_, count)| **count)
+                .min_by_key(|(id, count)| (std::cmp::Reverse(**count), *id))
                 .map(|(id, _)| id),
-            combos: vec![],
-            pa: use_counts
+            combos: [None; 8],
+            program_advance: use_counts
                 .iter()
                 .enumerate()
-                .filter(|(id, _)| {
+                .filter(|(id, count)| {
                     assets
                         .chip(*id)
                         .map(|c| c.class == rom::ChipClass::ProgramAdvance)
                         .unwrap_or(false)
+                        && **count > 0
                 })
-                .max_by_key(|(_, count)| **count)
+                .min_by_key(|(id, count)| (std::cmp::Reverse(**count), *id))
                 .map(|(id, _)| id),
         }
     }
+}
+
+fn show_table<const N: usize>(
+    ui: &mut egui::Ui,
+    chips: &[Option<usize>; N],
+    counts: &[usize; N],
+    assets: &Box<dyn rom::Assets + Send + Sync>,
+    font_families: &gui::FontFamilies,
+    lang: &unic_langid::LanguageIdentifier,
+    game_lang: &unic_langid::LanguageIdentifier,
+    chip_icon_texture_cache: &mut std::collections::HashMap<usize, egui::TextureHandle>,
+    element_icon_texture_cache: &mut std::collections::HashMap<usize, egui::TextureHandle>,
+) {
+    egui_extras::TableBuilder::new(ui)
+        .scroll(false)
+        .striped(true)
+        .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
+        .column(egui_extras::Size::exact(30.0))
+        .column(egui_extras::Size::exact(28.0))
+        .column(egui_extras::Size::remainder())
+        .column(egui_extras::Size::exact(28.0))
+        .column(egui_extras::Size::exact(30.0))
+        .body(|mut body| {
+            for (id, count) in std::iter::zip(chips, counts) {
+                body.row(28.0, |mut row| {
+                    row.col(|ui| {
+                        ui.strong(format!("{}x", count));
+                    });
+                    if let Some(id) = id {
+                        let info = assets.chip(*id);
+                        row.col(|ui| {
+                            let icon = if let Some(icon) = info.map(|info| &info.icon) {
+                                icon
+                            } else {
+                                return;
+                            };
+
+                            ui.image(
+                                chip_icon_texture_cache
+                                    .entry(*id)
+                                    .or_insert_with(|| {
+                                        ui.ctx().load_texture(
+                                            format!("chip {}", id),
+                                            egui::ColorImage::from_rgba_unmultiplied(
+                                                [14, 14],
+                                                &image::imageops::crop_imm(icon, 1, 1, 14, 14)
+                                                    .to_image(),
+                                            ),
+                                            egui::TextureFilter::Nearest,
+                                        )
+                                    })
+                                    .id(),
+                                egui::Vec2::new(28.0, 28.0),
+                            );
+                        });
+                        row.col(|ui| {
+                            ui.horizontal(|ui| {
+                                ui.horizontal(|ui| {
+                                    ui.label(
+                                        egui::RichText::new(
+                                            info.map(|info| info.name.as_str()).unwrap_or("???"),
+                                        )
+                                        .family(font_families.for_language(game_lang)),
+                                    );
+                                });
+                            });
+                        });
+                        row.col(|ui| {
+                            let element = if let Some(element) = info.map(|info| info.element) {
+                                element
+                            } else {
+                                return;
+                            };
+
+                            let icon = if let Some(icon) = assets.element_icon(element) {
+                                icon
+                            } else {
+                                return;
+                            };
+
+                            ui.image(
+                                element_icon_texture_cache
+                                    .entry(element)
+                                    .or_insert_with(|| {
+                                        ui.ctx().load_texture(
+                                            format!("element {}", element),
+                                            egui::ColorImage::from_rgba_unmultiplied(
+                                                [14, 14],
+                                                &image::imageops::crop_imm(icon, 1, 1, 14, 14)
+                                                    .to_image(),
+                                            ),
+                                            egui::TextureFilter::Nearest,
+                                        )
+                                    })
+                                    .id(),
+                                egui::Vec2::new(28.0, 28.0),
+                            );
+                        });
+                        row.col(|ui| {
+                            let damage = info.map(|info| info.damage).unwrap_or(0);
+                            ui.with_layout(
+                                egui::Layout::right_to_left(egui::Align::Center),
+                                |ui| {
+                                    if damage > 0 {
+                                        ui.strong(format!("{}", damage));
+                                    }
+                                },
+                            );
+                        });
+                    } else {
+                        row.col(|_ui| {});
+                        row.col(|ui| {
+                            ui.weak(i18n::LOCALES.lookup(lang, "dark-ai.unset").unwrap());
+                        });
+                        row.col(|_ui| {});
+                        row.col(|_ui| {});
+                    }
+                });
+            }
+        })
 }
 
 pub fn show<'a>(
@@ -131,4 +265,113 @@ pub fn show<'a>(
     let materialized = state
         .materialized
         .get_or_insert_with(|| MaterializedDarkAI::new(dark_ai_view, assets));
+    egui::ScrollArea::vertical()
+        .id_source("dark-ai-view")
+        .auto_shrink([false, false])
+        .show(ui, |ui| {
+            ui.push_id(
+                egui::Id::new("dark-ai-view-secondary-standard-chips"),
+                |ui| {
+                    ui.strong(
+                        i18n::LOCALES
+                            .lookup(lang, "dark-ai.secondary-standard-chips")
+                            .unwrap(),
+                    );
+                    show_table(
+                        ui,
+                        &materialized.secondary_standard_chips,
+                        &[1, 1, 1],
+                        assets,
+                        font_families,
+                        lang,
+                        game_lang,
+                        &mut state.chip_icon_texture_cache,
+                        &mut state.element_icon_texture_cache,
+                    );
+                },
+            );
+
+            ui.push_id(egui::Id::new("dark-ai-view-standard-chips"), |ui| {
+                ui.strong(
+                    i18n::LOCALES
+                        .lookup(lang, "dark-ai.standard-chips")
+                        .unwrap(),
+                );
+                show_table(
+                    ui,
+                    &materialized.standard_chips,
+                    &[4, 4, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                    assets,
+                    font_families,
+                    lang,
+                    game_lang,
+                    &mut state.chip_icon_texture_cache,
+                    &mut state.element_icon_texture_cache,
+                );
+            });
+
+            ui.push_id(egui::Id::new("dark-ai-view-mega-chips"), |ui| {
+                ui.strong(i18n::LOCALES.lookup(lang, "dark-ai.mega-chips").unwrap());
+                show_table(
+                    ui,
+                    &materialized.mega_chips,
+                    &[1, 1, 1, 1, 1],
+                    assets,
+                    font_families,
+                    lang,
+                    game_lang,
+                    &mut state.chip_icon_texture_cache,
+                    &mut state.element_icon_texture_cache,
+                );
+            });
+
+            ui.push_id(egui::Id::new("dark-ai-view-giga-chip"), |ui| {
+                ui.strong(i18n::LOCALES.lookup(lang, "dark-ai.giga-chip").unwrap());
+                show_table(
+                    ui,
+                    &[materialized.giga_chip],
+                    &[1],
+                    assets,
+                    font_families,
+                    lang,
+                    game_lang,
+                    &mut state.chip_icon_texture_cache,
+                    &mut state.element_icon_texture_cache,
+                );
+            });
+
+            ui.push_id(egui::Id::new("dark-ai-view-combos"), |ui| {
+                ui.strong(i18n::LOCALES.lookup(lang, "dark-ai.combos").unwrap());
+                show_table(
+                    ui,
+                    &[None; 8],
+                    &[1; 8],
+                    assets,
+                    font_families,
+                    lang,
+                    game_lang,
+                    &mut state.chip_icon_texture_cache,
+                    &mut state.element_icon_texture_cache,
+                );
+            });
+
+            ui.push_id(egui::Id::new("dark-ai-view-program-advance"), |ui| {
+                ui.strong(
+                    i18n::LOCALES
+                        .lookup(lang, "dark-ai.program-advance")
+                        .unwrap(),
+                );
+                show_table(
+                    ui,
+                    &[materialized.program_advance],
+                    &[1],
+                    assets,
+                    font_families,
+                    lang,
+                    game_lang,
+                    &mut state.chip_icon_texture_cache,
+                    &mut state.element_icon_texture_cache,
+                );
+            });
+        });
 }
