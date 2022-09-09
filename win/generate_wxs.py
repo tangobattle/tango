cf41b3899be13afdd6de5a8078f880f06a571fd3
bigfarts <bigfarts@punymail.com>
2022-09-08 19:12:25 -0700

    i HATE windows installer

--- win/generate_wxs.py
+++ win/generate_wxs.py
@@ -14,8 +14,8 @@ print(
 <?xml version="1.0" encoding="UTF-8"?>
 <Wix xmlns="http://schemas.microsoft.com/wix/2006/wi">
     <Product
-        Id="E8EA30F8-D1B3-4ABC-9048-F0BAD4835738"
-        UpgradeCode="*"
+        Id="*"
+        UpgradeCode="E8EA30F8-D1B3-4ABC-9048-F0BAD4835738"
         Language="1033"
         Manufacturer="Tango"
         Name="Tango"
@@ -23,7 +23,15 @@ print(
         <Package Id="*" InstallerVersion="200" InstallScope="perUser" />
         <Media Id="1" Cabinet="product.cab" EmbedCab="yes" />
         <Icon Id="icon.ico" SourceFile="icon.ico" />
-
+        <Upgrade Id="E8EA30F8-D1B3-4ABC-9048-F0BAD4835738">
+            <UpgradeVersion
+                OnlyDetect="no"
+                Minimum="0.0.0.0" Maximum="999.999.999.999"
+                Property="PREVIOUSVERSIONSINSTALLED"
+                IncludeMinimum="yes" IncludeMaximum="no" />
+        </Upgrade>
+        <Property Id="REINSTALLMODE" Value="dmus" />
+        <Property Id="PREVIOUSVERSIONSINSTALLED" />
         <Property Id="ARPPRODUCTICON" Value="icon.ico" />
         <Property Id="ALLUSERS" Value="2" />
         <Property Id="MSIINSTALLPERUSER" Value="1" />
@@ -76,7 +84,8 @@ print(
         </Feature>
 
         <InstallExecuteSequence>
-            <Custom Action="Launch" After="InstallFinalize" />
+            <RemoveExistingProducts After="InstallInitialize" />
+            <Custom Action="Launch" After="InstallFinalize">NOT Installed</Custom>
         </InstallExecuteSequence>
 
         <CustomAction
