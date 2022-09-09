e6ced566a317f01ec53a791a8d34719873d3f448
bigfarts <bigfarts@punymail.com>
2022-09-09 11:58:35 -0700

    remember to call language selector

--- tango/src/gui/settings_window.rs
+++ tango/src/gui/settings_window.rs
@@ -159,39 +159,7 @@ fn show_general_tab(ui: &mut egui::Ui, config: &mut config::Config, font_familie
 
             {
                 ui.strong(i18n::LOCALES.lookup(&config.language, "settings-language").unwrap());
-
-                let en_us_label = egui::RichText::new("English").family(font_families.latn.clone());
-                let ja_jp_label = egui::RichText::new("日本語").family(font_families.jpan.clone());
-                let zh_cn_label = egui::RichText::new("简体中文").family(font_families.hans.clone());
-                let zh_tw_label = egui::RichText::new("繁體中文").family(font_families.hant.clone());
-                let es_es_label = egui::RichText::new("Español").family(font_families.latn.clone());
-                let pt_br_label = egui::RichText::new("Português (Brasil)").family(font_families.latn.clone());
-                let fr_fr_label = egui::RichText::new("Français").family(font_families.latn.clone());
-                let de_de_label = egui::RichText::new("Deutsch").family(font_families.latn.clone());
-
-                egui::ComboBox::from_id_source("settings-window-general-language")
-                    .width(200.0)
-                    .selected_text(match &config.language {
-                        lang if lang.matches(&unic_langid::langid!("en-US"), false, true) => en_us_label.clone(),
-                        lang if lang.matches(&unic_langid::langid!("ja-JP"), false, true) => ja_jp_label.clone(),
-                        lang if lang.matches(&unic_langid::langid!("zh-CN"), false, true) => zh_cn_label.clone(),
-                        lang if lang.matches(&unic_langid::langid!("zh-TW"), false, true) => zh_tw_label.clone(),
-                        lang if lang.matches(&unic_langid::langid!("es-ES"), false, true) => es_es_label.clone(),
-                        lang if lang.matches(&unic_langid::langid!("pt-BR"), false, true) => pt_br_label.clone(),
-                        lang if lang.matches(&unic_langid::langid!("fr-FR"), false, true) => fr_fr_label.clone(),
-                        lang if lang.matches(&unic_langid::langid!("de-DE"), false, true) => de_de_label.clone(),
-                        _ => egui::RichText::new(""),
-                    })
-                    .show_ui(ui, |ui| {
-                        ui.selectable_value(&mut config.language, unic_langid::langid!("en-US"), en_us_label.clone());
-                        ui.selectable_value(&mut config.language, unic_langid::langid!("ja-JP"), ja_jp_label.clone());
-                        ui.selectable_value(&mut config.language, unic_langid::langid!("zh-CN"), zh_cn_label.clone());
-                        ui.selectable_value(&mut config.language, unic_langid::langid!("zh-TW"), zh_tw_label.clone());
-                        ui.selectable_value(&mut config.language, unic_langid::langid!("es-ES"), es_es_label.clone());
-                        ui.selectable_value(&mut config.language, unic_langid::langid!("pt-BR"), pt_br_label.clone());
-                        ui.selectable_value(&mut config.language, unic_langid::langid!("fr-FR"), fr_fr_label.clone());
-                        ui.selectable_value(&mut config.language, unic_langid::langid!("de-DE"), de_de_label.clone());
-                    });
+                gui::language_select::show(ui, font_families, &mut config.language);
                 ui.end_row();
             }
 
