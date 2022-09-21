405c5ca554db4157af6f12b2882422e4228d0892
bigfarts <bigfarts@punymail.com>
2022-09-21 00:33:20 -0700

    clean up language select

--- tango/src/gui/language_select.rs
+++ tango/src/gui/language_select.rs
@@ -1,42 +1,61 @@
 use crate::gui;
 
 pub fn show(ui: &mut egui::Ui, font_families: &gui::FontFamilies, language: &mut unic_langid::LanguageIdentifier) {
-    let en_us_label = egui::RichText::new("English (United States)").family(font_families.latn.egui.clone());
-    let ja_jp_label = egui::RichText::new("日本語").family(font_families.jpan.egui.clone());
-    let zh_cn_label = egui::RichText::new("简体中文").family(font_families.hans.egui.clone());
-    let zh_tw_label = egui::RichText::new("繁體中文").family(font_families.hant.egui.clone());
-    let es_419_label = egui::RichText::new("Español (Latinoamérica)").family(font_families.latn.egui.clone());
-    let pt_br_label = egui::RichText::new("Português (Brasil)").family(font_families.latn.egui.clone());
-    let fr_fr_label = egui::RichText::new("Français (France)").family(font_families.latn.egui.clone());
-    let de_de_label = egui::RichText::new("Deutsch (Deutschland)").family(font_families.latn.egui.clone());
-    let vi_vn_label = egui::RichText::new("Tiếng Việt").family(font_families.latn.egui.clone());
-    let ru_ru_label = egui::RichText::new("Русский (Россия)").family(font_families.latn.egui.clone());
+    let languages = &[
+        (
+            unic_langid::langid!("en-US"),
+            egui::RichText::new("English (United States)").family(font_families.latn.egui.clone()),
+        ),
+        (
+            unic_langid::langid!("ja-JP"),
+            egui::RichText::new("日本語").family(font_families.jpan.egui.clone()),
+        ),
+        (
+            unic_langid::langid!("zh-CN"),
+            egui::RichText::new("简体中文").family(font_families.hans.egui.clone()),
+        ),
+        (
+            unic_langid::langid!("zh-TW"),
+            egui::RichText::new("繁體中文").family(font_families.hant.egui.clone()),
+        ),
+        (
+            unic_langid::langid!("es-419"),
+            egui::RichText::new("Español (Latinoamérica)").family(font_families.latn.egui.clone()),
+        ),
+        (
+            unic_langid::langid!("pt-BR"),
+            egui::RichText::new("Português (Brasil)").family(font_families.latn.egui.clone()),
+        ),
+        (
+            unic_langid::langid!("fr-FR"),
+            egui::RichText::new("Français (France)").family(font_families.latn.egui.clone()),
+        ),
+        (
+            unic_langid::langid!("de-DE"),
+            egui::RichText::new("Deutsch (Deutschland)").family(font_families.latn.egui.clone()),
+        ),
+        (
+            unic_langid::langid!("vi-VN"),
+            egui::RichText::new("Tiếng Việt").family(font_families.latn.egui.clone()),
+        ),
+        (
+            unic_langid::langid!("ru-RU"),
+            egui::RichText::new("Русский (Россия)").family(font_families.latn.egui.clone()),
+        ),
+    ];
 
     egui::ComboBox::from_id_source("settings-window-general-language")
         .width(200.0)
-        .selected_text(match &language {
-            lang if lang.matches(&unic_langid::langid!("en-US"), false, true) => en_us_label.clone(),
-            lang if lang.matches(&unic_langid::langid!("ja-JP"), false, true) => ja_jp_label.clone(),
-            lang if lang.matches(&unic_langid::langid!("zh-CN"), false, true) => zh_cn_label.clone(),
-            lang if lang.matches(&unic_langid::langid!("zh-TW"), false, true) => zh_tw_label.clone(),
-            lang if lang.matches(&unic_langid::langid!("es-419"), false, true) => es_419_label.clone(),
-            lang if lang.matches(&unic_langid::langid!("pt-BR"), false, true) => pt_br_label.clone(),
-            lang if lang.matches(&unic_langid::langid!("fr-FR"), false, true) => fr_fr_label.clone(),
-            lang if lang.matches(&unic_langid::langid!("de-DE"), false, true) => de_de_label.clone(),
-            lang if lang.matches(&unic_langid::langid!("vi-VN"), false, true) => vi_vn_label.clone(),
-            lang if lang.matches(&unic_langid::langid!("ru-RU"), false, true) => ru_ru_label.clone(),
-            _ => egui::RichText::new(""),
-        })
+        .selected_text(
+            languages
+                .iter()
+                .find(|(lang, _)| language.matches(lang, false, true))
+                .map(|(_, label)| label.clone())
+                .unwrap_or_else(|| egui::RichText::new("")),
+        )
         .show_ui(ui, |ui| {
-            ui.selectable_value(language, unic_langid::langid!("en-US"), en_us_label.clone());
-            ui.selectable_value(language, unic_langid::langid!("ja-JP"), ja_jp_label.clone());
-            ui.selectable_value(language, unic_langid::langid!("zh-CN"), zh_cn_label.clone());
-            ui.selectable_value(language, unic_langid::langid!("zh-TW"), zh_tw_label.clone());
-            ui.selectable_value(language, unic_langid::langid!("es-419"), es_419_label.clone());
-            ui.selectable_value(language, unic_langid::langid!("pt-BR"), pt_br_label.clone());
-            ui.selectable_value(language, unic_langid::langid!("fr-FR"), fr_fr_label.clone());
-            ui.selectable_value(language, unic_langid::langid!("de-DE"), de_de_label.clone());
-            ui.selectable_value(language, unic_langid::langid!("vi-VN"), vi_vn_label.clone());
-            ui.selectable_value(language, unic_langid::langid!("ru-RU"), ru_ru_label.clone());
+            for (lang, label) in languages.iter() {
+                ui.selectable_value(language, lang.clone(), label.clone());
+            }
         });
 }
