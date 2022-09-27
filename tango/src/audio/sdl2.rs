4491052b7e9323539396922207e0ec1b8a5d074c
bigfarts <bigfarts@punymail.com>
2022-09-26 20:03:59 -0700

    Revert "remove sdl2 audio backend"
    This reverts commit 89e92614e9062fda6151e7a7815dd5f8f4cafb1e.

---
+++ tango/src/audio/sdl2.rs
@@ -0,0 +1,45 @@
+use crate::audio;
+
+pub struct StreamWrapper(Box<dyn audio::Stream + Send + 'static>);
+
+impl sdl2::audio::AudioCallback for StreamWrapper {
+    type Channel = i16;
+
+    fn callback(&mut self, buf: &mut [i16]) {
+        let frame_count = self.0.fill(bytemuck::cast_slice_mut(buf));
+        for x in &mut buf[frame_count * audio::NUM_CHANNELS..] {
+            use sdl2::audio::AudioFormatNum;
+            *x = Self::Channel::SILENCE;
+        }
+    }
+}
+
+pub struct Backend {
+    _audio: sdl2::AudioSubsystem,
+    _audio_device: sdl2::audio::AudioDevice<StreamWrapper>,
+}
+
+impl Backend {
+    pub fn new(sdl: &sdl2::Sdl, stream: impl audio::Stream + Send + 'static) -> Result<Self, anyhow::Error> {
+        let audio = sdl.audio().map_err(|e| anyhow::format_err!("{}", e))?;
+        let audio_device = audio
+            .open_playback(
+                None,
+                &sdl2::audio::AudioSpecDesired {
+                    freq: Some(48000),
+                    channels: Some(audio::NUM_CHANNELS as u8),
+                    samples: Some(audio::SAMPLES as u16),
+                },
+                |_| StreamWrapper(Box::new(stream)),
+            )
+            .map_err(|e| anyhow::format_err!("{}", e))?;
+        log::info!("sdl2 audio spec: {:?}", audio_device.spec());
+        audio_device.resume();
+        Ok(Self {
+            _audio: audio,
+            _audio_device: audio_device,
+        })
+    }
+}
+
+impl audio::Backend for Backend {}
