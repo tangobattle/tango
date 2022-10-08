db976ba2d0b2c06a8b74eb76fa239c6e060eff5a
bigfarts <bigfarts@punymail.com>
2022-10-08 09:32:51 -0700

    whoops

--- tango/src/gui/session_view/replay_controls_window.rs
+++ tango/src/gui/session_view/replay_controls_window.rs
@@ -47,11 +47,10 @@ pub fn show(
                 {
                     speed = std::cmp::max_by(speed - 0.25, 0.25, |x, y| x.partial_cmp(y).unwrap());
                 }
-                ui.add(egui::Slider::new(&mut speed, 0.25..=10.0).step_by(0.25))
-                    .on_hover_text(i18n::LOCALES.lookup(language, "replay-viewer-speed-up").unwrap());
+                ui.add(egui::Slider::new(&mut speed, 0.25..=10.0).step_by(0.25));
                 if ui
                     .button("🐇")
-                    .on_hover_text(i18n::LOCALES.lookup(language, "replay-viewer-pause").unwrap())
+                    .on_hover_text(i18n::LOCALES.lookup(language, "replay-viewer-speed-up").unwrap())
                     .clicked()
                 {
                     speed = std::cmp::min_by(speed + 0.25, 10.0, |x, y| x.partial_cmp(y).unwrap());
