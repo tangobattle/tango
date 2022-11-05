7815f344948474ef69893f9df47a54a578aba78b
bigfarts <bigfarts@punymail.com>
2022-11-05 13:43:50 -0700

    only compile resource.rc if available

--- tango/build.rs
+++ tango/build.rs
@@ -9,7 +9,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
 
     if target_os == "windows" {
-        embed_resource::compile("resource.rc");
+        match std::fs::metadata("resource.rc") {
+            Ok(_) => {
+                embed_resource::compile("resource.rc");
+            }
+            Err(e) if e.kind() == std::io::ErrorKind::NotFound => {}
+            Err(e) => {
+                return Err(Box::new(e));
+            }
+        }
     }
 
     Ok(())
