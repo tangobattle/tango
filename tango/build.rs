29f3c4a07085dbda18b3e806ca3abf63db3f03b4
bigfarts <bigfarts@punymail.com>
2022-11-05 21:12:31 -0700

    add replay dump tools

--- tango/build.rs
+++ tango/build.rs
@@ -1,7 +1,21 @@
 extern crate embed_resource;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    prost_build::compile_protos(
+    let mut prost_config = prost_build::Config::new();
+    prost_config.type_attribute("tango.replay.protos.replay11.Metadata", "#[derive(serde::Serialize)]");
+    prost_config.type_attribute(
+        "tango.replay.protos.replay11.Metadata.Side",
+        "#[derive(serde::Serialize)]",
+    );
+    prost_config.type_attribute(
+        "tango.replay.protos.replay11.Metadata.GameInfo",
+        "#[derive(serde::Serialize)]",
+    );
+    prost_config.type_attribute(
+        "tango.replay.protos.replay11.Metadata.GameInfo.Patch",
+        "#[derive(serde::Serialize)]",
+    );
+    prost_config.compile_protos(
         &["src/replay/protos/replay11.proto", "src/replay/protos/replay10.proto"],
         &["src/"],
     )?;
--- tango/src/replaytool.rs
+++ tango/src/replaytool.rs
@@ -1,8 +1,12 @@
+use std::io::Write;
+
 use crate::{config, replay};
 
 #[derive(clap::Subcommand)]
 pub enum Command {
     Invert { output_path: std::path::PathBuf },
+    Metadata,
+    Wram,
     Text,
 }
 
@@ -12,6 +16,8 @@ pub fn main(config: config::Config, path: std::path::PathBuf, command: Command) 
 
     match command {
         Command::Invert { output_path } => cmd_invert(config, replay, output_path),
+        Command::Metadata => cmd_metadata(config, replay),
+        Command::Wram => cmd_wram(config, replay),
         Command::Text => cmd_text(config, replay),
     }
 }
@@ -44,5 +50,18 @@ fn cmd_text(_config: config::Config, replay: replay::Replay) -> Result<(), anyho
             ip.local.local_tick, ip.local.joyflags, ip.local.packet, ip.remote.joyflags, ip.remote.packet,
         );
     }
+    Ok(())
+}
+
+fn cmd_metadata(_config: config::Config, replay: replay::Replay) -> Result<(), anyhow::Error> {
+    let mut stdout = std::io::stdout().lock();
+    serde_json::to_writer_pretty(&mut stdout, &replay.metadata)?;
+    stdout.write_all(b"\n")?;
+    Ok(())
+}
+
+fn cmd_wram(_config: config::Config, replay: replay::Replay) -> Result<(), anyhow::Error> {
+    let mut stdout = std::io::stdout().lock();
+    stdout.write_all(replay.local_state.wram())?;
     Ok(())
 }
