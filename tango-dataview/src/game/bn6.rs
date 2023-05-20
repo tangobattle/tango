02978b9f8ea7a82d672a4f46f7447a94d93a6326
bigfarts <bigfarts@punymail.com>
2023-05-20 08:03:46 -0700

    fix bn6 chip number

--- tango-dataview/src/game/bn6.rs
+++ tango-dataview/src/game/bn6.rs
@@ -1,6 +1,6 @@
 pub mod rom;
 pub mod save;
 
-const NUM_CHIPS: usize = 368;
+const NUM_CHIPS: usize = 411;
 const NUM_PATCH_CARD56S: usize = 118;
 const NUM_NAVICUST_PARTS: (usize, usize) = (47, 4);
