ea256d882d8aad792d08d97e93ed79029e35499c
bigfarts <bigfarts@punymail.com>
2023-06-20 14:27:00 -0700

    just disable mgba logging

--- mgba/src/log.rs
+++ mgba/src/log.rs
@@ -1,32 +1,3 @@
-unsafe fn vsprintf<VaList>(fmt: *const std::os::raw::c_char, args: VaList) -> std::ffi::CString {
-    const INITIAL_BUF_SIZE: usize = 512;
-    let mut buf = vec![0u8; INITIAL_BUF_SIZE];
-
-    loop {
-        let n: usize = mgba_sys::vsnprintf(
-            buf.as_mut_ptr() as *mut _,
-            buf.len() as u64,
-            fmt,
-            std::mem::transmute_copy(&args),
-        )
-        .try_into()
-        .unwrap();
-
-        if n + 1 <= buf.len() {
-            break;
-        }
-        buf.resize(n + 1, 0);
-    }
-
-    match std::ffi::CString::new(vsprintf(fmt, args)) {
-        Ok(r) => r,
-        Err(err) => {
-            let nul_pos = err.nul_position();
-            std::ffi::CString::new(&err.into_vec()[0..nul_pos]).unwrap()
-        }
-    }
-}
-
 unsafe extern "C" fn c_log<VaList>(
     _logger: *mut mgba_sys::mLogger,
     category: i32,
@@ -34,20 +5,20 @@ unsafe extern "C" fn c_log<VaList>(
     fmt: *const std::os::raw::c_char,
     args: VaList,
 ) {
-    log::log!(
-        match level {
-            mgba_sys::mLogLevel_mLOG_STUB => log::Level::Trace,
-            mgba_sys::mLogLevel_mLOG_DEBUG => log::Level::Debug,
-            mgba_sys::mLogLevel_mLOG_INFO => log::Level::Info,
-            mgba_sys::mLogLevel_mLOG_WARN => log::Level::Warn,
-            mgba_sys::mLogLevel_mLOG_ERROR | mgba_sys::mLogLevel_mLOG_FATAL | mgba_sys::mLogLevel_mLOG_GAME_ERROR =>
-                log::Level::Error,
-            _ => log::Level::Info,
-        },
-        "{}: {}",
-        std::ffi::CStr::from_ptr(mgba_sys::mLogCategoryName(category)).to_string_lossy(),
-        vsprintf(fmt, args).to_string_lossy()
-    );
+    // log::log!(
+    //     match level {
+    //         mgba_sys::mLogLevel_mLOG_STUB => log::Level::Trace,
+    //         mgba_sys::mLogLevel_mLOG_DEBUG => log::Level::Debug,
+    //         mgba_sys::mLogLevel_mLOG_INFO => log::Level::Info,
+    //         mgba_sys::mLogLevel_mLOG_WARN => log::Level::Warn,
+    //         mgba_sys::mLogLevel_mLOG_ERROR | mgba_sys::mLogLevel_mLOG_FATAL | mgba_sys::mLogLevel_mLOG_GAME_ERROR =>
+    //             log::Level::Error,
+    //         _ => log::Level::Info,
+    //     },
+    //     "{}: {}",
+    //     std::ffi::CStr::from_ptr(mgba_sys::mLogCategoryName(category)).to_string_lossy(),
+    //     todo!()
+    // );
 }
 
 #[repr(transparent)]
