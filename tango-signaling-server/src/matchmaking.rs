98e68083e5c9f8588483329b5c3ddce71430b3e3
bigfarts <bigfarts@punymail.com>
2023-07-27 01:55:32 -0700

    enforce protocol check

--- tango-signaling-server/src/matchmaking.rs
+++ tango-signaling-server/src/matchmaking.rs
@@ -131,6 +131,29 @@ impl Server {
             }
         };
 
+        const EXPECTED_PROTOCOL_VERSION: u8 = 0x37;
+        if start.protocol_version as u8 != EXPECTED_PROTOCOL_VERSION {
+            tokio::time::timeout(
+                TX_TIMEOUT,
+                tx.send(tungstenite::Message::Binary(
+                    tango_signaling::proto::signaling::Packet {
+                        which: Some(tango_signaling::proto::signaling::packet::Which::Abort(
+                            tango_signaling::proto::signaling::packet::Abort {
+                                reason: if (start.protocol_version as u8) < EXPECTED_PROTOCOL_VERSION {
+                                    tango_signaling::proto::signaling::packet::abort::Reason::ProtocolVersionTooOld
+                                } else {
+                                    tango_signaling::proto::signaling::packet::abort::Reason::ProtocolVersionTooNew
+                                } as i32,
+                            },
+                        )),
+                    }
+                    .encode_to_vec(),
+                )),
+            )
+            .await??;
+            return Ok(());
+        }
+
         let tx = std::sync::Arc::new(tokio::sync::Mutex::new(tx));
 
         let offerer_tx = {
